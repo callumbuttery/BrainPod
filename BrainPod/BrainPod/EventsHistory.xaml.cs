561fe5811e34cf0d9f9fa2f0c6c7f2b0fac310c1
Callum Buttery <40408762@live.napier.ac.uk>
2020-12-09 14:00:35 +0000

    added event history functionality

--- BrainPod/BrainPod/EventsHistory.xaml.cs
+++ BrainPod/BrainPod/EventsHistory.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BrainPod.Table;
+using Firebase.Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,11 +17,55 @@ namespace BrainPod
         //global
         Guid uID;
 
+        //firebase connection
+        public static FirebaseClient firebaseClient = new FirebaseClient("https://brainpod-eba39.firebaseio.com/");
+
         public EventsHistory(Guid id)
         {
             InitializeComponent();
 
             uID = id;
+
+            //get all events
+            getEventHistory(id);
+        }
+
+        async void getEventHistory(Guid id)
+        {
+            try
+            {
+                //list to store events returned from backend
+                List<newEvent> eventlog = new List<newEvent>();
+
+                //get history from user ID
+                //searches for criteria where logs have been updated
+                eventlog.Clear();
+                eventlog = (await firebaseClient
+                    .Child("newEvent")
+                    .OnceAsync<newEvent>()).Where(a => a.Object.userID == id).Select(item => new newEvent
+                    {
+                        userID = item.Object.userID,
+                        EventID = item.Object.EventID,
+                        eventTitle = item.Object.eventTitle,
+                        eventDate = item.Object.eventDate,
+                        reason = item.Object.reason,
+                        anxietyLevel = item.Object.anxietyLevel,
+                        worryLevel = item.Object.worryLevel,
+                        factsfeeling = item.Object.factsfeeling,
+                        scenarioEvaluation = item.Object.scenarioEvaluation,
+                        positiveNotes = item.Object.positiveNotes,
+
+
+                    }).ToList();
+
+                //set list source
+                listOfEvents.ItemsSource = eventlog;
+            }
+            catch(Exception ex)
+            {
+                //display error
+                await DisplayAlert("Uh oh", "Please show this to support: \n\n" + ex.ToString(), "Okay");
+            }
         }
     }
 }
--- BrainPod/BrainPod/obj/Debug/netstandard2.0/EventsHistory.xaml.g.cs
+++ BrainPod/BrainPod/obj/Debug/netstandard2.0/EventsHistory.xaml.g.cs
@@ -17,12 +17,12 @@ namespace BrainPod {
     public partial class EventsHistory : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
-        private global::Xamarin.Forms.ListView listOfLogs;
+        private global::Xamarin.Forms.ListView listOfEvents;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(EventsHistory));
-            listOfLogs = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ListView>(this, "listOfLogs");
+            listOfEvents = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ListView>(this, "listOfEvents");
         }
     }
 }
